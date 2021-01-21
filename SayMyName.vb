68b364024d7a6c132b53426e72bd6f56d918af07
Gilcspen@gmail.com <Gilcspen@gmail.com>
2021-01-21 08:30:59 -0700

    Add project files.

---
+++ SayMyName.vb
@@ -0,0 +1,31 @@
+﻿
+'Spencer Gilchrist
+'RCET0265
+'Spring 2021
+'Say My Name
+'
+
+Option Explicit On
+Option Strict On
+Module SayMyName
+
+    Sub Main()
+
+        Console.WriteLine("Say Your Name!")
+
+        Dim UserInput As String
+
+        UserInput = Console.ReadLine()
+        Console.WriteLine($"Hello, {UserInput} How was your day today?")
+        System.Threading.Thread.Sleep(1500)
+        Console.WriteLine("Was it Good or Bad?")
+
+        UserInput = Console.ReadLine()
+
+        Console.WriteLine($"That's {UserInput}.")
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
